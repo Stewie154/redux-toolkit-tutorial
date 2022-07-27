38f6ce3656ae6f9dd4acf1976a7c97c38cc4a8bf
Stewart McFarlane <stewart@toru.digital>
2022-07-27 20:14:12 +0100

    clearCart action

--- starter/src/components/CartContainer.js
+++ starter/src/components/CartContainer.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import CartItem from './CartItem'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { clearCart } from '../features/cart/cartSlice'
 
 const CartContainer = () => {
 	const { cartItems, total, amount } = useSelector(store => store.cart)
+
+	const dispatch = useDispatch()
 
 	if (amount < 1) {
 		return (
@@ -32,7 +35,7 @@ const CartContainer = () => {
 						total <span>${total}</span>
 					</h4>
 				</div>
-				<button className="btn clear-btn">clear cart</button>
+				<button className="btn clear-btn" onClick={() => dispatch(clearCart())}>clear cart</button>
 			</footer>
 		</section>
 	)
--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { ChevronDown, ChevronUp } from '../icons'
+import {useDispatch, useSelector} from 'react-redux'
 
 const cartItem = ({id, img, title, price, amount}) => {
+		
 	return (
 		<article className="cart-item">
 			<img src={img} alt={title}/>
