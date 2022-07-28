8b09af60c645a2c3419028686e9d249a26851ee7
Stewart McFarlane <stewart@toru.digital>
2022-07-28 08:24:52 +0100

    created Modal component

--- starter/src/App.js
+++ starter/src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { calculateTotals } from './features/cart/cartSlice';
 
+import Modal from './components/Modal';
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 
@@ -15,6 +16,7 @@ function App() {
 
 	return (
 		<main>
+			<Modal />
 			<Navbar />
 			<CartContainer />
 		</main>
